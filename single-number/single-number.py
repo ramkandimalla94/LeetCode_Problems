25127e556da016817a1856bd4ddb153723d0868e
ramkandimalla <37137053+ramkandimalla94@users.noreply.github.com>
2022-08-22 19:38:36 -0500

    Time: 249 ms, Memory: 15.3 MB - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -4,21 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #print(nums)
         nums.sort()
-        #print(nums)
         
         if len(nums) == 1:
             return nums[0]
         else:
-#             if len(nums)%2 == 1:
-#                 for i in range(1,len(nums),2):
-
-#                     if nums[i]!=nums[i-1]:
-#                         return nums[i-1]
-#             else:
             for i in range(0,len(nums),2):
-                #print(nums[i])
                 try:
                     
                     if nums[i]!=nums[i+1]:
