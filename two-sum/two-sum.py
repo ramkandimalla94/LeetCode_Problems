8eb35a064e74af5b2e59d79c5b95a6b94501cb2b
ramkandimalla <37137053+ramkandimalla94@users.noreply.github.com>
2022-08-24 20:40:51 -0500

    Time: 702 ms, Memory: 13.9 MB - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -5,18 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        import numpy as np
-        
-        #nums= np.array(nums)
-        #nums_original = nums.copy()
-        #nums.sort()
         
         for i in range(len(nums)):
-            
-            #print(i,target - nums[i])
-            
+                 
             if target - nums[i] in nums[i+1:] :
                 
                 return (i,nums[i+1:].index(target-nums[i])+i+1)
-                #return (np.where(nums_original==nums[i])[0][0],np.where(nums_original[i+1:]==target - nums[i])[0][0])
+                
                 
